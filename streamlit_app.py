f160b2d4c9a982dd1000f4767ded8e07babd6970
Nurdhuha <nurdhuhaam@gmail.com>
2025-01-17 10:54:05 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,7 @@ def handle_user_action():
                     user_data[username] = password
                     save_user_data(user_data)
                     st.success("Sign Up sukses.")
+                    st.switch_page("pages/menu_input_data.py")
             else:
                 st.error("Silakan masukkan username dan password.")
 
@@ -51,6 +52,7 @@ def handle_user_action():
                     st.success("Login berhasil!")
                     st.session_state.logged_in = True
                     st.session_state.username = username
+                    st.success("Login Berhasil.")
                     st.switch_page("pages/menu_input_data.py")
             else:
                 st.error("Silakan masukkan username dan password.")
